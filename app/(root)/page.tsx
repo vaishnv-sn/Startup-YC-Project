edb7715ec57681c29c69bb3651454e6a3bb1b4d1
vaishnv-sn <vaishnav.sn4@gmail.com>
2024-12-08 20:34:48 +0530

    Configured the statup card in hero section with sample content

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,21 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _id: 7,
+      _createdAt: new Date(),
+      views: 34,
+      author: { _id: 1, name: `Elon Musk` },
+      description: "We're the future of Robotics engineering",
+      image:
+        "https://futureskillsprime.in/sites/default/files/inline-images/iStock-966248982.jpg",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -17,6 +33,21 @@ export default async function Home({
           Submit Ideas, Vote on Pitches, and Get Noticed on Virtual Competitions
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search Results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups Found</p>
+          )}
+        </ul>
       </section>
     </>
   );
